331d66fb6147137cd30c1bc175be7f6bbbe7dee9
RTSR <delavalierre@gmail.com>
2022-08-04 00:49:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GO_Launcher_UpdateGen
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFilesPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectOutputPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFilePathInput = new System.Windows.Forms.TextBox();
+            this.selectFilesPathLabel = new System.Windows.Forms.Label();
+            this.selectFilePathBrowse = new System.Windows.Forms.Button();
+            this.selectOutputPathInput = new System.Windows.Forms.TextBox();
+            this.selectOutputPathBrowse = new System.Windows.Forms.Button();
+            this.selectOutputPathLabel = new System.Windows.Forms.Label();
+            this.startUpdateGenButton = new System.Windows.Forms.Button();
+            this.resultString = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.versionInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // selectFilePathInput
+            // 
+            this.selectFilePathInput.Enabled = false;
+            this.selectFilePathInput.Location = new System.Drawing.Point(12, 79);
+            this.selectFilePathInput.Name = "selectFilePathInput";
+            this.selectFilePathInput.Size = new System.Drawing.Size(189, 20);
+            this.selectFilePathInput.TabIndex = 0;
+            // 
+            // selectFilesPathLabel
+            // 
+            this.selectFilesPathLabel.AutoSize = true;
+            this.selectFilesPathLabel.Location = new System.Drawing.Point(12, 63);
+            this.selectFilesPathLabel.Name = "selectFilesPathLabel";
+            this.selectFilesPathLabel.Size = new System.Drawing.Size(154, 13);
+            this.selectFilesPathLabel.TabIndex = 1;
+            this.selectFilesPathLabel.Text = "Select folder with updated files:";
+            // 
+            // selectFilePathBrowse
+            // 
+            this.selectFilePathBrowse.Location = new System.Drawing.Point(207, 77);
+            this.selectFilePathBrowse.Name = "selectFilePathBrowse";
+            this.selectFilePathBrowse.Size = new System.Drawing.Size(75, 23);
+            this.selectFilePathBrowse.TabIndex = 2;
+            this.selectFilePathBrowse.Text = "Browse";
+            this.selectFilePathBrowse.UseVisualStyleBackColor = true;
+            this.selectFilePathBrowse.Click += new System.EventHandler(this.selectFilePathBrowse_Click);
+            // 
+            // selectOutputPathInput
+            // 
+            this.selectOutputPathInput.Enabled = false;
+            this.selectOutputPathInput.Location = new System.Drawing.Point(12, 128);
+            this.selectOutputPathInput.Name = "selectOutputPathInput";
+            this.selectOutputPathInput.Size = new System.Drawing.Size(189, 20);
+            this.selectOutputPathInput.TabIndex = 3;
+            // 
+            // selectOutputPathBrowse
+            // 
+            this.selectOutputPathBrowse.Location = new System.Drawing.Point(207, 125);
+            this.selectOutputPathBrowse.Name = "selectOutputPathBrowse";
+            this.selectOutputPathBrowse.Size = new System.Drawing.Size(75, 23);
+            this.selectOutputPathBrowse.TabIndex = 4;
+            this.selectOutputPathBrowse.Text = "Browse";
+            this.selectOutputPathBrowse.UseVisualStyleBackColor = true;
+            this.selectOutputPathBrowse.Click += new System.EventHandler(this.selectOutputPathBrowse_Click);
+            // 
+            // selectOutputPathLabel
+            // 
+            this.selectOutputPathLabel.AutoSize = true;
+            this.selectOutputPathLabel.Location = new System.Drawing.Point(9, 112);
+            this.selectOutputPathLabel.Name = "selectOutputPathLabel";
+            this.selectOutputPathLabel.Size = new System.Drawing.Size(145, 13);
+            this.selectOutputPathLabel.TabIndex = 5;
+            this.selectOutputPathLabel.Text = "Select folder for output result:";
+            // 
+            // startUpdateGenButton
+            // 
+            this.startUpdateGenButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.startUpdateGenButton.Location = new System.Drawing.Point(0, 202);
+            this.startUpdateGenButton.Name = "startUpdateGenButton";
+            this.startUpdateGenButton.Size = new System.Drawing.Size(291, 38);
+            this.startUpdateGenButton.TabIndex = 6;
+            this.startUpdateGenButton.Text = "Start";
+            this.startUpdateGenButton.UseVisualStyleBackColor = true;
+            this.startUpdateGenButton.Click += new System.EventHandler(this.startUpdateGenButton_Click);
+            // 
+            // resultString
+            // 
+            this.resultString.Location = new System.Drawing.Point(12, 151);
+            this.resultString.Name = "resultString";
+            this.resultString.Size = new System.Drawing.Size(270, 32);
+            this.resultString.TabIndex = 7;
+            this.resultString.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Location = new System.Drawing.Point(12, 17);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(218, 13);
+            this.versionLabel.TabIndex = 8;
+            this.versionLabel.Text = "Input version (not needed for additional files):";
+            // 
+            // versionInput
+            // 
+            this.versionInput.Location = new System.Drawing.Point(12, 33);
+            this.versionInput.MaxLength = 24;
+            this.versionInput.Name = "versionInput";
+            this.versionInput.Size = new System.Drawing.Size(186, 20);
+            this.versionInput.TabIndex = 9;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 240);
+            this.Controls.Add(this.versionInput);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.resultString);
+            this.Controls.Add(this.startUpdateGenButton);
+            this.Controls.Add(this.selectOutputPathLabel);
+            this.Controls.Add(this.selectOutputPathBrowse);
+            this.Controls.Add(this.selectOutputPathInput);
+            this.Controls.Add(this.selectFilePathBrowse);
+            this.Controls.Add(this.selectFilesPathLabel);
+            this.Controls.Add(this.selectFilePathInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Launcher Update Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog selectFilesPath;
+        private System.Windows.Forms.FolderBrowserDialog selectOutputPath;
+        private System.Windows.Forms.TextBox selectFilePathInput;
+        private System.Windows.Forms.Label selectFilesPathLabel;
+        private System.Windows.Forms.Button selectFilePathBrowse;
+        private System.Windows.Forms.TextBox selectOutputPathInput;
+        private System.Windows.Forms.Button selectOutputPathBrowse;
+        private System.Windows.Forms.Label selectOutputPathLabel;
+        private System.Windows.Forms.Button startUpdateGenButton;
+        private System.Windows.Forms.Label resultString;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.TextBox versionInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace GO_Launcher_UpdateGen
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFilePathBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = selectFilesPath.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(selectFilesPath.SelectedPath))
+            {
+                selectFilePathInput.Text = selectFilesPath.SelectedPath;
+            }
+        }
+
+        private void selectOutputPathBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = selectOutputPath.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(selectOutputPath.SelectedPath))
+            {
+                selectOutputPathInput.Text = selectOutputPath.SelectedPath;
+            }
+        }
+
+        public static List<String> GetAllFiles(string directory)
+        {
+            return Directory.GetFiles(directory, "*", SearchOption.AllDirectories).ToList();
+        }
+
+        public static string GetFileMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        private void startUpdateGenButton_Click(object sender, EventArgs e)
+        {
+            if (selectFilesPath.SelectedPath.Length != 0 && selectOutputPath.SelectedPath.Length != 0)
+            {
+                resultString.ForeColor = Color.FromArgb(255, 0, 0, 0);
+                // Creating files directory
+                if (!Directory.Exists(selectOutputPath.SelectedPath + "/files/"))
+                    Directory.CreateDirectory(selectOutputPath.SelectedPath + "/files/");
+
+                // Creating XML document
+                XmlDocument doc         = new XmlDocument();
+                XmlElement rootElement  = (XmlElement)doc.AppendChild(doc.CreateElement("file_list"));
+
+                // Processing file list
+                var fileList = GetAllFiles(selectFilesPath.SelectedPath);
+                foreach(string path in fileList)
+                {
+                    resultString.Text = "Processing " + Path.GetFileName(path) + "...";
+
+                    XmlElement fileElement = (XmlElement)rootElement.AppendChild(doc.CreateElement("file"));
+
+                    // Generating MD5
+                    string md5 = GetFileMD5(path);
+                    fileElement.AppendChild(doc.CreateElement("md5")).InnerText         = md5;
+
+                    // Generating file name
+                    fileElement.AppendChild(doc.CreateElement("fileName")).InnerText    = Path.GetFileName(path);
+
+                    // Generating file url
+                    fileElement.AppendChild(doc.CreateElement("fileUrl")).InnerText     = Path.GetFileName(path);
+
+                    // Generating file path
+                    Uri originalPathUri = new Uri(selectFilesPath.SelectedPath + "\\");
+                    Uri filePathUri     = new Uri(path);
+                    Uri diff            = originalPathUri.MakeRelativeUri(filePathUri);
+                    fileElement.AppendChild(doc.CreateElement("filePath")).InnerText    = diff.OriginalString;
+
+                    File.Copy(path, selectOutputPath.SelectedPath + "\\files\\" + Path.GetFileName(path), true);
+                }
+
+                resultString.ForeColor  = Color.FromArgb(255, 0, 255, 0);
+                resultString.Text       = "Update successfully generated!";
+                doc.Save(selectOutputPath.SelectedPath + "/files.xml");
+
+                // Generating version.xml
+                if (versionInput.Text.Length != 0)
+                {
+                    doc = new XmlDocument();
+                    rootElement = (XmlElement)doc.AppendChild(doc.CreateElement("info"));
+                    rootElement.AppendChild(doc.CreateElement("version")).InnerText = versionInput.Text;
+                    doc.Save(selectOutputPath.SelectedPath + "/version.xml");
+                }
+            }
+        }
+    }
+}
